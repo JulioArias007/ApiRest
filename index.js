5d74ad672ff6fedcf1129066c22cb416c2656513
Julio Cesar Arias <julio.arias007@hotmail.com>
2017-06-19 15:41:46 -0500

    Curso API REST con NodeJS y MongoDB Video 10
    Cómo implementar DELETE y UPDATE de registros en tu API REST | Curso de
    NodeJS y MongoDB #10

--- index.js
+++ index.js
@@ -43,22 +43,38 @@ app.post('/api/product', (req, res) => {
   product.category = req.body.category
   product.description = req.body.description
 
-  product.save((err,productStored) =>{
-    if(err) res.status(500).send({message:`Error al salvar en la base de datos: ${err} `})
+  product.save((err, productStored) => {
+    if(err) res.status(500).send({message: `Error al salvar en la base de datos: ${err} `})
 
     res.status(200).send({product: productStored})
   })
 })
 
 app.put('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId
+  let update = req.body
 
+  Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+    if(err) res.status(500).send({message: `Error al actualizar en la base de datos: ${err} `})
+
+    res.status(200).send({product: productUpdated})
+  })
 })
 
 app.delete('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId
 
+  Product.findById(productId, (err, product) => {
+    if(err) res.status(500).send({message: `Error al borrar el producto: ${err} `})
+
+    product.remove(err => {
+      if(err) res.status(500).send({message: `Error al borrar el producto: ${err} `})
+      res.status(200).send({message: 'El producto ha sido eliminado'})
+    })
+  })
 })
 
-mongoose.connect('mongodb://localhost:27017/shop',(err, res)=>{
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
   if(err) {
     return console.log(`Error al conectar a la base de datos: ${err}`)
   }
